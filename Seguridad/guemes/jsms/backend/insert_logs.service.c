ad2698ae86a5ee48a4f990eabe8a9e3f339ebcea
jose luis bisconti <jose.bisconti@gsventerprise.com.ar>
2024-08-09 09:28:46 -0300

    Update insert_logs.service.c

--- Seguridad/guemes/jsms/backend/insert_logs.service.c
+++ Seguridad/guemes/jsms/backend/insert_logs.service.c
@@ -1,10 +1,12 @@
-[Unit] Description = Servicio para ejecutar insert_logs
- After = network.target
+[Unit]
+Description=Servicio para ejecutar insert_logs
+After=network.target
 
-[Service] 
-ExecStart = /home/jlb/venv/bin/python3 /home/jlb/insert_logs.py 
-WorkingDirectory =/home/jlb/
-Restart = always
-User = root
+[Service]
+ExecStart=/home/jlb/venv/bin/python3 /home/jlb/insert_logs.py
+WorkingDirectory=/home/jlb/
+Restart=always
+User=root
 
-[Install] WantedBy =multi-user.target
+[Install]
+WantedBy=multi-user.target
