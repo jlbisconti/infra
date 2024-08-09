904bced5972a422aea4d364c9217bda3effe940c
murdok2023 <jose.bisconti@gsventerprise.com.ar>
2024-08-09 09:27:08 -0300

    insert_logs.service

---
+++ Seguridad/guemes/jsms/backend/insert_logs.service.c
@@ -0,0 +1,10 @@
+[Unit] Description = Servicio para ejecutar insert_logs
+ After = network.target
+
+[Service] 
+ExecStart = /home/jlb/venv/bin/python3 /home/jlb/insert_logs.py 
+WorkingDirectory =/home/jlb/
+Restart = always
+User = root
+
+[Install] WantedBy =multi-user.target
